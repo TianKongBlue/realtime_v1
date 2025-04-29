38ce0c6ea1f5731e82ad466101cf28b6effe1e0b
zhnengyang.zhang <3098992152@qq.com>
2025-04-29 10:56:22 +0800

    新建

---
+++ realtime-gmall/realtime-dwd/src/main/java/com/yz/app/NewProject.java
@@ -0,0 +1,16 @@
+package com.yz.app;/**
+ * /**
+ *
+ * @Package com.yz.app.NewProject
+ * @Author zhengyang.zhang
+ * @Date 2025/4/29 10:48
+ *
+ */
+
+
+public class NewProject {
+    public static void main(String[] args) {
+        System.out.println("How are you");
+    }
+}
+
